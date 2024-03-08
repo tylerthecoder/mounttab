caaad2e55d28c863835a6cced2f653a40e270b9f
Tyler Tracy <tylertracy1999@gmail.com>
2024-03-07 22:48:47 -0600

    Help option + DX

--- src/cli.ts
+++ src/cli.ts
@@ -4,6 +4,14 @@ import { TabService } from "./state";
 
 const command = process.argv[2];
 
+function printHelp() {
+    console.log("Commands:");
+    console.log("  start <workspace>");
+    console.log("  serve");
+    console.log("  list-workspaces");
+    console.log("  read-state");
+}
+
 if (command === "start") {
     const workspace = process.argv[3];
     await fetch(`http://localhost:${Config.serverPort}/start?workspace=${workspace}`)
@@ -12,7 +20,10 @@ if (command === "start") {
 } else if (command == "list-workspaces") {
     const worksapces = await TabService.getAllWorkspaces();
     console.log(worksapces.join("\n"));
+} else if (command == "read-state") {
+    const state = await TabService.getFromFs();
+    console.log(JSON.stringify(state, null, 2));
 } else {
-    console.log("Command not valid");
+    printHelp();
 }
 
